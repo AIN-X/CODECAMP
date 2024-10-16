fef09dc7132171edd76015ca50fcc6b8b35d7af6
AIN-X <ashrafule44@gmail.com>
2024-10-16 23:37:09 +0600

    Journey was started

---
+++ hi.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+cout<<"\nHello , World!"<<endl;
+
+cout<<"\nI am Ashraful and my coding Journey is Start....!\n"<<endl;
+
+
+    return 0;
+}
