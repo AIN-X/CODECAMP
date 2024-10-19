481028427b0cb6a7b57c9a01b64924b7b6f193d7
AIN-X <ashrafule44@gmail.com>
2024-10-19 23:09:57 +0600

    Using dowhile loop

---
+++ Lesson 3/Dowhile.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int i=0;
+
+    int sum=0;
+
+    do{
+    sum=sum+i ;
+    i++;
+    }
+
+while(i<10);
+
+{
+    cout<<"\nSum is:  "<<sum<<"\n"<<endl;
+
+}
+}
