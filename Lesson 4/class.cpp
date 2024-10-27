32e84b24323344ae001ffcf36b60cd9e993036c2
AIN-X <ashrafule44@gmail.com>
2024-10-27 18:26:10 +0600

    call1

---
+++ Lesson 4/class.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void change_Salary(int s)
+{
+
+    s = 40000;
+
+    cout << "\nChange on functionk : " << s << endl;
+}
+int main()
+{
+    int sal = 27000;
+    change_Salary(sal);
+    cout << "\nSalary inside main : " << sal<<"\n "<< endl;
+}
--- Lesson 4/function.cpp
+++ Lesson 4/function.cpp
@@ -1,20 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+void swap(int x, int y)
+{
+    int temp = x;
+    x = y;
+    y = temp;
+}
+int main()
+{
 
-void swap(int x,int y){
-    int temp=x;
-    x=y;
-     y=temp;
-     }
-int main(){  
+    int x = 150;
+    int y = 200;
+    swap(x, y);
 
-    int x= 150;
-    int y= 200;
-    swap(x,y);
+    cout << " The X value is : " << x << endl;
+    cout << " The Y value is : " << y << endl;
 
-    cout<<" The X value is : "<<x<<endl;
-    cout<<" The Y value is : "<<y<<endl;
-        
-            return 0;
+    return 0;
 }
